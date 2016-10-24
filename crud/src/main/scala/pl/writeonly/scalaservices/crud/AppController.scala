709258997121a6f4fe48aef5374792653960a575
Kamil Adam <kamil.adam.zabinski@gmail.com>
2016-10-24 21:04:51 +0200

    AppController

--- crud/src/main/scala/pl/writeonly/scalaservices/crud/AppController.scala
+++ crud/src/main/scala/pl/writeonly/scalaservices/crud/AppController.scala
@@ -1,19 +1,24 @@
 package pl.writeonly.scalaservices.crud
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+
+import org.springframework.stereotype.Controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+
 object AppController extends App {
-  SpringApplication.run(SampleController.class, args);
+  SpringApplication.run(classOf[AppController]);
 }
 
 @Controller
 @EnableAutoConfiguration
 class AppController {
 
-    @RequestMapping("/")
+    @RequestMapping(Array("crud/"))
     @ResponseBody
-    def home = "Hello World!";
+    def crud = "CRUD";
     
-
-    public static void main(String[] args) throws Exception {
-     
-    }
 }
