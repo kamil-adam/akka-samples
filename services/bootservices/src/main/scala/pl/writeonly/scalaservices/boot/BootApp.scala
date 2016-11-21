7ab111fe32f5f67c33597d853831faec67874511
Kamil Adam <kamil.adam.zabinski@gmail.com>
2016-11-21 20:52:30 +0100

    connection

--- services/bootservices/src/main/scala/pl/writeonly/scalaservices/boot/BootApp.scala
+++ services/bootservices/src/main/scala/pl/writeonly/scalaservices/boot/BootApp.scala
@@ -2,7 +2,7 @@ package pl.writeonly.scalaservices.boot
 
 import org.springframework.boot.SpringApplication
 
-//import pl.writeonly.scalaservices.boot.domain._
+import pl.writeonly.scalaservices.boot.domain.ConnectionService
 
 object BootApp extends App {
   SpringApplication.run(classOf[ConnectionService]);
--- services/bootservices/src/main/scala/pl/writeonly/scalaservices/boot/domain/Connection.scala
+++ services/bootservices/src/main/scala/pl/writeonly/scalaservices/boot/domain/Connection.scala
@@ -1,4 +1,4 @@
-package pl.writeonly.scalaservices.boot
+package pl.writeonly.scalaservices.boot.domain
 
 class Connection(var url : String, var username : String, var password : String) {
   
